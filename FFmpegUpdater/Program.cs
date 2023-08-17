b76794b9d8617fc23d18ef6d92fcd8c495474e6a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-08-17 12:12:47 +0800

    尝试添加新的方式

--- FFmpegUpdater/Program.cs
+++ FFmpegUpdater/Program.cs
@@ -23,7 +23,82 @@ string currentDirectory = Directory.GetCurrentDirectory();
 ZipFile.ExtractToDirectory(zipFilePath, currentDirectory);
 
 // 下面开始添加Path
+string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+string ffmpegBinPath = Path.Combine(appDirectory, "ffmpeg", "bin");
 
+Console.WriteLine("请选择要添加路径的类型：");
+Console.WriteLine("1. 用户路径");
+Console.WriteLine("2. 系统路径");
+Console.Write("请输入选项（1 或 2）：");
+
+string option = Console.ReadLine();
+if (option == "1")
+{
+    if (!IsDirectoryInPath(ffmpegBinPath, Environment.GetEnvironmentVariable("PATH")))
+    {
+        AddDirectoryToPath(ffmpegBinPath, EnvironmentVariableTarget.User);
+        Console.WriteLine("目录已成功添加到用户路径环境变量中。");
+    }
+    else
+    {
+    Console.WriteLine("目录已存在于用户路径环境变量中。");
+    }
+}
+else if (option == "2")
+{
+    if (!IsDirectoryInPath(ffmpegBinPath, Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine)))
+    {
+        AddDirectoryToPath(ffmpegBinPath, EnvironmentVariableTarget.Machine);
+        Console.WriteLine("目录已成功添加到系统路径环境变量中。");
+    }
+    else
+    {
+        Console.WriteLine("目录已存在于系统路径环境变量中。");
+    }
+}
+else
+{
+    Console.WriteLine("无效的选项！");
+}
+
+    Console.ReadLine();
+
+static bool IsDirectoryInPath(string directory, string pathVariable)
+{
+    string[] paths = pathVariable.Split(';');
+
+    foreach (string path in paths)
+    {
+        if (string.Equals(path.Trim(), directory, StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static void AddDirectoryToPath(string directory, EnvironmentVariableTarget target)
+{
+    string pathVariable = Environment.GetEnvironmentVariable("PATH", target);
+    pathVariable = $"{directory};{pathVariable}";
+
+    // 启动一个新进程来设置环境变量
+    using (Process process = new Process())
+    {
+        process.StartInfo.FileName = "cmd.exe";
+        process.StartInfo.Arguments = $"/C setx PATH \"{pathVariable}\" /{GetTargetString(target)}";
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.Start();
+        process.WaitForExit();
+    }
+}
+
+static string GetTargetString(EnvironmentVariableTarget target)
+{
+    return target == EnvironmentVariableTarget.Machine ? "M" : "U";
+}
 
 
 
